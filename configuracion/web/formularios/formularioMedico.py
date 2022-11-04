89ffd11e573c092c881421b78ef1f82f0f80cee3
509 <509@academicos.cesde.edu.co>
2022-11-04 17:37:13 -0500

    se crean formularios datos del profesional

--- configuracion/web/formularios/formularioMedico.py
+++ configuracion/web/formularios/formularioMedico.py
@@ -2,30 +2,62 @@ from django import forms
 class FormularioMedico(forms.Form):
 
     ESPECIALIDADES=(
-        (1,'Cardiologia')
-        (2,'Medicina Interna')
-        (3,'Medico General')
-        (4,'Ortopedia')
+        (1,'Cardiologia'),
+        (2,'Medicina Interna'),
+        (3,'Medico General'),
+        (4,'Ortopedia'),
         (5,'Pediatria')
 
     )
     JORNADAS=(
-        (1, '6:00 - 14:00')
-        (2, '14:00 - 22:00')
+        (1, '6:00 - 14:00'),
+        (2, '14:00 - 22:00'),
         (3, '22:00 - 6:00')
     )
     SEDES=(
-        (1, 'Almacentro')
-        (2, 'Punto Clave')
+        (1, 'Almacentro'),
+        (2, 'Punto Clave'),
         (3, 'Molinos')
     )
 
-    nombre=forms.CharField()
-    apellidos=forms.CharField()
-    cedula=forms.CharField()
-    tarjetaProfesional=forms.CharField()
-    especialidad=forms.ChoiceField
-    jornada=forms.ChoiceField
-    Contacto=forms.CharField() 
-    sede=forms.ChoiceField
+    nombre=forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
+        required=True,
+        max_length=15
+    )
+    apellidos=forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
+        required=True,
+        max_length=15
+    )
+    cedula=forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
+        required=True,
+        max_length=15
+    )
+    tarjeta_Profesional=forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
+        required=True,
+        max_length=15
+    )
+    especialidad=forms.ChoiceField(
+        widget=forms.Select(attrs={"class":"form-select mb-3"}),
+        required=True,
+        choices=ESPECIALIDADES
+    )
+    jornada=forms.ChoiceField(
+        widget=forms.Select(attrs={"class":"form-select mb-3"}),
+        required=True,
+        choices=JORNADAS
+    )
+    Contacto=forms.CharField(
+        widget=forms.TextInput(attrs={"class":"form-control mb-3"}),
+        required=True,
+        max_length=15
+    ) 
+    sede=forms.ChoiceField(
+                widget=forms.Select(attrs={"class":"form-select mb-3"}),
+        required=True,
+        choices=SEDES
+    )
 
--- configuracion/web/views.py
+++ configuracion/web/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from web.formularios.formularioMedico import FormularioMedico
 
 # Create your views here.
 #render (renderizar) es pintar
@@ -6,4 +7,20 @@ def Home(request):
     return render(request,'index.html')
 
 def Medicos(request):
-    return render(request,'registrosmedicos.html')
+    #debo utilizar la clase de formulario medico
+    #creamos entonces un obejto
+    formulario=FormularioMedico()
+    diccionario={
+        "formulario":formulario
+    }
+    #activar la recepcion de datos
+    if request.method=='POST':
+        #validar si los datos son correctos
+        datosRecibidos=FormularioMedico(request.POST)
+        if datosRecibidos.is_valid():
+            #capturar los datos
+            datos=datosRecibidos.cleaned_data
+            print(datos)
+
+
+    return render(request,'registrosmedicos.html',diccionario)
